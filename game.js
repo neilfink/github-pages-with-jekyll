0b52b69fde883d5dd7337ad72846b5e82a14bf93
Neil F <43327004+neilfink@users.noreply.github.com>
2021-09-16 03:34:43 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,23 @@
+kaboom({
+    global: true,
+    fullscreen: true,
+    width: 320,
+    height: 240,
+    stretch: true,
+    letterbox: true,
+    clearColor: [ 0, 0, 255],
+});
+
+loadRoot('https://i.imgur.com/');
+loadSprite('ship','7poQcIP.png');
+
+const player = add([
+    sprite('ship'),
+    pos(160,120),
+]) ;
+
+const scoreLabel = add([
+    text("Test label"),
+    scale(0.5),
+    pos(40, 8),
+])
