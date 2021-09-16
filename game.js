c6bb60a675311b86810aa01a3d5d295b3384fede
Neil F <43327004+neilfink@users.noreply.github.com>
2021-09-16 04:10:53 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,22 +1,23 @@
-kaboom({
-    global: true,
-    width: 320,
-    height: 240,
-    stretch: true,
-    letterbox: true,
-    clearColor: [ 0, 0, 0],
-});
+            kaboom({
+                stretch: true,
+                letterbox: true,
+                width: 320,
+                height: 240,
+                scale: 3,
+                font: "sinko",
+                clearColor: [ 60, 0, 30, ],
+            });
 
-loadRoot('https://i.imgur.com/');
-loadSprite('ship','7poQcIP.png');
+            loadRoot('https://i.imgur.com/');
+            loadSprite('ship','7poQcIP.png');
 
-const player = add([
-    sprite('ship'),
-    pos(160,120),
-]) ;
+            const player = add([
+                sprite('ship'),
+                pos(160,120),
+            ]) ;
 
-const scoreLabel = add([
-    text("Test label"),
-    scale(0.5),
-    pos(40, 8),
-])
+            const scoreLabel = add([
+                text("Test label"),
+                scale(1),
+                pos(40, 8),
+            ])
